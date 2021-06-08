d29b10ead70347e15e6425f30075ac941e062a51
Mohamed Khazbak <mohamedkhazbak99@gmail.com>
2021-06-08 19:04:02 +0200

    MoodSwing

---
+++ lib/models/FrameInfo.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FrameInfo {
+  String email = "khazbak@gmail.com";
+  String videoid = "video495";
+  var emotions = [];
+  FrameInfo();
+  FrameInfo.id(email, videoid) {
+    this.email = email;
+    this.videoid = videoid;
+  }
+
+  Future<dynamic> getFrameInfo() async {
+    print("in get frame info function");
+    DocumentSnapshot variable = await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(email)
+        .collection("VideoModelOutput")
+        .doc("video495")
+        .get();
+    emotions = variable.get("Mood");
+    print("after document snapshot");
+    print(emotions);
+    return emotions;
+  }
+}
