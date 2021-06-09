2ec369249669fb07fd8fa3f55f42dd284617c40b
Mohamed Khazbak <mohamedkhazbak99@gmail.com>
2021-06-09 21:10:31 +0200

    doctor

---
+++ lib/widgets/viewdrform.dart
@@ -0,0 +1,315 @@
+import 'package:date_field/date_field.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:medico/widgets/admin/editdr.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
+import 'package:medico/models/users.dart';
+import 'package:medico/pages/EditProfile.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+class ViewdrForm extends StatefulWidget {
+  @override
+  ViewdrFormState createState() {
+    return ViewdrFormState();
+  }
+}
+
+//DocumentSnapshot variable;
+
+class ViewdrFormState extends State<ViewdrForm> {
+  String doctoremail;
+
+  final namecontroller = TextEditingController();
+  final locationcontoller = TextEditingController();
+  final descriptioncontroller = TextEditingController();
+  final passwordcontroller = TextEditingController();
+  final newpasswordcontroller = TextEditingController();
+  var _passwordVisible;
+  FocusNode myFocusNode;
+
+  void getdoctoremail() async {
+    print('object');
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    //doctoremail = prefs.getString('doctoremail');
+    doctoremail = 'hamada@mindtime.com';
+    DocumentSnapshot variable = await FirebaseFirestore.instance
+        .collection('Therapists')
+        .doc(doctoremail)
+        .get();
+    if (variable.data() != null) {
+      print('object1');
+      setState(() {
+        this.namecontroller.text = variable.get("name");
+        this.descriptioncontroller.text = variable.get("description");
+        this.locationcontoller.text = variable.get("location");
+
+        this.passwordcontroller.text = variable.get("password");
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    _passwordVisible = false;
+    super.initState();
+    getdoctoremail();
+    //getdetails();
+    myFocusNode = FocusNode();
+  }
+
+  final _formKey = GlobalKey<FormState>();
+  static final validCharactersPassword = RegExp(r'^[a-zA-Z0-9_\-=@\.;{6,}+$]');
+  String convertDateTimeDisplay(String date) {
+    final DateFormat displayFormater = DateFormat('yyyy-MM-dd HH:mm:ss.SSS');
+    final DateFormat serverFormater = DateFormat('dd/MM/yyyy');
+    final DateTime displayDate = displayFormater.parse(date);
+    final String formatted = serverFormater.format(displayDate);
+    return formatted;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime selectedDate;
+
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(padding: EdgeInsets.only(top: 280.0)),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(21.0),
+              color: const Color(0xffffffff),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0x21329d9c),
+                  offset: Offset(0, 13),
+                  blurRadius: 34,
+                ),
+              ],
+            ),
+            child: TextFormField(
+              // initialValue: name,
+              controller: namecontroller,
+
+              style: TextStyle(
+                color: Theme.of(context).accentColor,
+              ),
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.perm_identity),
+                  labelText: 'Enter therapist name',
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).accentColor,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Theme.of(context).accentColor,
+                  )),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Theme.of(context).accentColor,
+                    ),
+                    borderRadius: BorderRadius.circular(21.0),
+                  )),
+              /* validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+              },*/
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(top: 10.0)),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(21.0),
+              color: const Color(0xffffffff),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0x21329d9c),
+                  offset: Offset(0, 13),
+                  blurRadius: 34,
+                ),
+              ],
+            ),
+            child: TextFormField(
+              // initialValue: variable.get("location"),
+              controller: locationcontoller,
+
+              style: TextStyle(
+                color: Theme.of(context).accentColor,
+              ),
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.perm_identity),
+                  labelText: 'new location',
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).accentColor,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Theme.of(context).accentColor,
+                  )),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Theme.of(context).accentColor,
+                    ),
+                    borderRadius: BorderRadius.circular(21.0),
+                  )),
+              /*validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                //return null;
+              },*/
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(top: 10.0)),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(21.0),
+              color: const Color(0xffffffff),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0x21329d9c),
+                  offset: Offset(0, 13),
+                  blurRadius: 34,
+                ),
+              ],
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(top: 10.0)),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(21.0),
+              color: const Color(0xffffffff),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0x21329d9c),
+                  offset: Offset(0, 13),
+                  blurRadius: 34,
+                ),
+              ],
+            ),
+            child: TextFormField(
+              // initialValue: variable.get("description"),
+              controller: descriptioncontroller,
+
+              style: TextStyle(
+                color: Theme.of(context).accentColor,
+              ),
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.perm_identity),
+                  labelText: 'Description',
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).accentColor,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Theme.of(context).accentColor,
+                  )),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Theme.of(context).accentColor,
+                    ),
+                    borderRadius: BorderRadius.circular(21.0),
+                  )),
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(top: 10.0)),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(21.0),
+              color: const Color(0xffffffff),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0x21329d9c),
+                  offset: Offset(0, 13),
+                  blurRadius: 34,
+                ),
+              ],
+            ),
+            child: TextFormField(
+              controller: passwordcontroller,
+              obscureText: !_passwordVisible,
+              style: TextStyle(
+                color: Theme.of(context).accentColor,
+              ),
+              decoration: InputDecoration(
+                  prefixIcon: IconButton(
+                    icon: Icon(
+                      _passwordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                      color: Theme.of(context).primaryColorDark,
+                    ),
+                    onPressed: () {
+                      // Update the state i.e. toogle the state of passwordVisible variable
+                      setState(() {
+                        _passwordVisible = !_passwordVisible;
+                      });
+                    },
+                  ),
+                  labelText: 'Old Password',
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).accentColor,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Theme.of(context).accentColor,
+                  )),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Theme.of(context).accentColor,
+                    ),
+                    borderRadius: BorderRadius.circular(21.0),
+                  )),
+              /* validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },*/
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(top: 10.0)),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(21.0),
+              color: const Color(0xffffffff),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0x21329d9c),
+                  offset: Offset(0, 13),
+                  blurRadius: 34,
+                ),
+              ],
+            ),
+          ),
+          Padding(padding: const EdgeInsets.symmetric(vertical: 5.0)),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(21.0),
+              gradient: new LinearGradient(
+                  colors: [
+                    Theme.of(context).accentColor,
+                    Theme.of(context).accentColor
+                  ],
+                  begin: const FractionalOffset(0.1, 1.0),
+                  end: const FractionalOffset(0.8, 0.5),
+                  stops: [0.0, 1.0],
+                  tileMode: TileMode.clamp),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0x36329d9c),
+                  offset: Offset(15, 15),
+                  blurRadius: 40,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
